486961deb1f199318b64ae6a61db0a327c70fa0b
jyoti140220 <jyotibhandari20@navgurukul.org>
2021-06-24 10:15:12 +0530

    new

---
+++ indexOfVovel.js
@@ -0,0 +1,11 @@
+const readlinesync=require('readline-sync').question
+var str=readlinesync("enetr the string :---- ")
+arr1=[]
+for (i in str){
+    if (str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u"){
+        var index=str.indexOf(str[i])
+        arr1.push(`Letter :- ${str[i]} Index :- ${index}`)
+    
+    }
+}
+console.log(arr1);
